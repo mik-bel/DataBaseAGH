a82780246bcc6397f363b82714e896ea0d8b0a5b
mik-bel <mikita.yakimovich@gmail.com>
2018-01-08 02:53:08 +0100

    CHECKS where added

--- Conferance.sql
+++ Conferance.sql
@@ -15,7 +15,8 @@ CREATE TABLE Adresses (
     City nvarchar(20)  NOT NULL,
     FstLine nvarchar(40)  NOT NULL,
     ScdLine nvarchar(40)  NOT NULL,
-    CONSTRAINT Adresses_pk PRIMARY KEY  (AdressID)
+    CONSTRAINT Adresses_pk PRIMARY KEY  (AdressID),
+	CONSTRAINT PostalCode_clength_check CHECK (len(PostalCode) BETWEEN 1 AND 8)
 );
 
 CREATE INDEX AdressID on Adresses (AdressID ASC)
@@ -33,7 +34,8 @@ CREATE TABLE Clients (
     Login nchar(10)  NOT NULL,
     Password nvarchar(255)  NOT NULL,
     Mail varchar(60)  NOT NULL,
-    CONSTRAINT Clients_pk PRIMARY KEY  (ClientID)
+    CONSTRAINT Clients_pk PRIMARY KEY  (ClientID),
+	CONSTRAINT PostalCode_clength_check CHECK (Mail like '%_@_%_._%'),
 );
 
 CREATE INDEX ClientID on Clients (ClientID ASC,CompanyID ASC)
@@ -50,7 +52,8 @@ CREATE TABLE Companies (
     CompanyName nvarchar(20)  NOT NULL,
     AdressID int  NOT NULL,
     NIP nvarchar(30)  NOT NULL,
-    CONSTRAINT Companies_pk PRIMARY KEY CLUSTERED (CompanyID ASC)
+    CONSTRAINT Companies_pk PRIMARY KEY CLUSTERED (CompanyID ASC),
+	CONSTRAINT NIP_format_check CHECK (NIP like '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
 );
 
 CREATE INDEX Companies_idx_1 on Companies (CompanyID ASC)
@@ -86,7 +89,9 @@ CREATE TABLE ConfDayReservations (
     ReservationDate date  NOT NULL,
     PaidPrice float  NOT NULL,
     NumStudents int  NOT NULL,
-    CONSTRAINT ConfDayReservations_pk PRIMARY KEY  (ConfDayReservationID)
+    CONSTRAINT ConfDayReservations_pk PRIMARY KEY  (ConfDayReservationID),
+	CONSTRAINT NumSeats_num_ckeck CHECK (NumSeats>0),
+	CONSTRAINT NumStudents_not_more_then_NumSeats_check CHECK (NumStudents<=NumSeats)
 );
 
 CREATE INDEX ConfDayReservations_idx_1 on ConfDayReservations (ConfDayReservationID ASC)
@@ -102,6 +107,7 @@ CREATE TABLE Conference (
     ID_Conference int  NOT NULL,
     Name varchar(50)  NOT NULL,
     AdressID int  NOT NULL,
+	StudentDiscount int NOT NULL,
     CONSTRAINT Conference_pk PRIMARY KEY  (ID_Conference)
 );
 
@@ -120,7 +126,8 @@ CREATE TABLE ConferenceDays (
     Date date  NOT NULL,
     Price float  NOT NULL,
     SeatsNum int  NOT NULL,
-    CONSTRAINT ConferenceDays_pk PRIMARY KEY  (ConfDayID)
+    CONSTRAINT ConferenceDays_pk PRIMARY KEY  (ConfDayID),
+	CONSTRAINT SeatsNum_not_zero CHECK (SeatsNum>0)
 );
 
 CREATE INDEX ConferenceDays_idx_1 on ConferenceDays (ConfDayID ASC)
@@ -138,7 +145,9 @@ CREATE TABLE Discounts (
     DiscountStartDate date  NOT NULL,
     DiscountEndDate date  NOT NULL,
     Discount float  NOT NULL,
-    CONSTRAINT Discounts_pk PRIMARY KEY  (DiscountID)
+    CONSTRAINT Discounts_pk PRIMARY KEY  (DiscountID),
+	CONSTRAINT Start_End_Discount_Date_check CHECK (DiscountStartDate<DiscountEndDate),
+	CONSTRAINT Discount_format_check CHECK (Discount between 1 and 99)
 );
 
 CREATE INDEX Discounts_idx_1 on Discounts (DiscountID ASC)
@@ -157,7 +166,9 @@ CREATE TABLE Participants (
     LastName nvarchar(20)  NOT NULL,
     AdressID int  NOT NULL,
     StudentCardID int  NULL,
-    CONSTRAINT Participants_pk PRIMARY KEY  (ParticipantID)
+    CONSTRAINT Participants_pk PRIMARY KEY (ParticipantID),
+	CONSTRAINT FirstName_Format CHECK (FirstName NOT LIKE '%[^a-zA-Z,.\-\ ]%'),
+	CONSTRAINT LastName_Format CHECK (LastName NOT LIKE '%[^a-zA-Z,.\-\ ]%')
 );
 
 CREATE INDEX Participants_idx_1 on Participants (ParticipantID ASC)
@@ -193,7 +204,9 @@ CREATE TABLE WorkshopReservations (
     NumReservs int  NOT NULL,
     ReservationDate date  NOT NULL,
     NumStudents int  NOT NULL,
-    CONSTRAINT WorkshopReservations_pk PRIMARY KEY  (WorkshopReservID)
+    CONSTRAINT WorkshopReservations_pk PRIMARY KEY  (WorkshopReservID),
+	CONSTRAINT NumReservs_check CHECK (NumReservs > 0),
+	CONSTRAINT NumSrudent_not_greater_then_Reservs_ckeck CHECK (NumStudents <= NumReservs)
 );
 
 CREATE INDEX WorkshopReservations_idx_1 on WorkshopReservations (WorkshopReservID ASC)
@@ -213,7 +226,8 @@ CREATE TABLE Workshops (
     StartTime time  NOT NULL,
     EndTime time  NOT NULL,
     Price float  NOT NULL,
-    CONSTRAINT Workshops_pk PRIMARY KEY  (WorkshopID)
+    CONSTRAINT Workshops_pk PRIMARY KEY  (WorkshopID),
+	CONSTRAINT Start_End_time_check CHECK (StartTime<EndTime)
 );
 
 CREATE INDEX Workshops_idx_1 on Workshops (WorkshopID ASC)
@@ -224,88 +238,103 @@ CREATE INDEX Workshops_idx_1 on Workshops (WorkshopID ASC)
 ALTER TABLE Clients ADD CONSTRAINT Clients_Companies
     FOREIGN KEY (CompanyID)
     REFERENCES Companies (CompanyID)
-    ON UPDATE  CASCADE;
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Companies_Adresses (table: Companies)
 ALTER TABLE Companies ADD CONSTRAINT Companies_Adresses
     FOREIGN KEY (AdressID)
-    REFERENCES Adresses (AdressID);
+    REFERENCES Adresses (AdressID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: ConfDayRegistration_Participants (table: ConfDayRegistrations)
 ALTER TABLE ConfDayRegistrations ADD CONSTRAINT ConfDayRegistration_Participants
     FOREIGN KEY (ParticipantID)
-    REFERENCES Participants (ParticipantID);
+    REFERENCES Participants (ParticipantID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: ConfDayReservation_Clients (table: ConfDayReservations)
 ALTER TABLE ConfDayReservations ADD CONSTRAINT ConfDayReservation_Clients
     FOREIGN KEY (ClientID)
-    REFERENCES Clients (ClientID);
+    REFERENCES Clients (ClientID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: ConfDayReservation_ConfDayRegistration (table: ConfDayRegistrations)
 ALTER TABLE ConfDayRegistrations ADD CONSTRAINT ConfDayReservation_ConfDayRegistration
     FOREIGN KEY (ConfDayReservationID)
-    REFERENCES ConfDayReservations (ConfDayReservationID);
+    REFERENCES ConfDayReservations (ConfDayReservationID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: ConfDayReservation_ConferenceDays (table: ConfDayReservations)
 ALTER TABLE ConfDayReservations ADD CONSTRAINT ConfDayReservation_ConferenceDays
     FOREIGN KEY (ConfDayID)
-    REFERENCES ConferenceDays (ConfDayID);
+    REFERENCES ConferenceDays (ConfDayID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Conference_Adresses (table: Conference)
 ALTER TABLE Conference ADD CONSTRAINT Conference_Adresses
     FOREIGN KEY (AdressID)
-    REFERENCES Adresses (AdressID);
+    REFERENCES Adresses (AdressID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Discounts_ConferenceDays (table: Discounts)
 ALTER TABLE Discounts ADD CONSTRAINT Discounts_ConferenceDays
     FOREIGN KEY (ConfDayID)
-    REFERENCES ConferenceDays (ConfDayID);
+    REFERENCES ConferenceDays (ConfDayID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Konferencje_Dni_Konferencje (table: ConferenceDays)
 ALTER TABLE ConferenceDays ADD CONSTRAINT Konferencje_Dni_Konferencje
     FOREIGN KEY (ConfID)
-    REFERENCES Conference (ID_Conference);
+    REFERENCES Conference (ID_Conference)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Participants_Adresses (table: Participants)
 ALTER TABLE Participants ADD CONSTRAINT Participants_Adresses
     FOREIGN KEY (AdressID)
-    REFERENCES Adresses (AdressID);
+    REFERENCES Adresses (AdressID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Participants_Clients (table: Participants)
 ALTER TABLE Participants ADD CONSTRAINT Participants_Clients
     FOREIGN KEY (ClientID)
     REFERENCES Clients (ClientID)
-    ON UPDATE  CASCADE;
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: WorkshopRegistrations_ConfDayRegistrations (table: WorkshopRegistrations)
 ALTER TABLE WorkshopRegistrations ADD CONSTRAINT WorkshopRegistrations_ConfDayRegistrations
     FOREIGN KEY (ConfDayRegistrationID)
-    REFERENCES ConfDayRegistrations (ConfDayRegistrationID);
+    REFERENCES ConfDayRegistrations (ConfDayRegistrationID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: WorkshopRegistrations_Participants (table: WorkshopRegistrations)
 ALTER TABLE WorkshopRegistrations ADD CONSTRAINT WorkshopRegistrations_Participants
     FOREIGN KEY (ParticipantID)
-    REFERENCES Participants (ParticipantID);
+    REFERENCES Participants (ParticipantID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: WorkshopRegistrations_WorkshopReservations (table: WorkshopRegistrations)
 ALTER TABLE WorkshopRegistrations ADD CONSTRAINT WorkshopRegistrations_WorkshopReservations
     FOREIGN KEY (WorkshopReservID)
-    REFERENCES WorkshopReservations (WorkshopReservID);
+    REFERENCES WorkshopReservations (WorkshopReservID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: WorkshopReservations_ConfDayReservations (table: WorkshopReservations)
 ALTER TABLE WorkshopReservations ADD CONSTRAINT WorkshopReservations_ConfDayReservations
     FOREIGN KEY (ConfDayReservationID)
-    REFERENCES ConfDayReservations (ConfDayReservationID);
+    REFERENCES ConfDayReservations (ConfDayReservationID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: WorkshopReservations_Workshops (table: WorkshopReservations)
 ALTER TABLE WorkshopReservations ADD CONSTRAINT WorkshopReservations_Workshops
     FOREIGN KEY (WorkshopID)
-    REFERENCES Workshops (WorkshopID);
+    REFERENCES Workshops (WorkshopID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Reference: Workshops_ConferenceDays (table: Workshops)
 ALTER TABLE Workshops ADD CONSTRAINT Workshops_ConferenceDays
     FOREIGN KEY (ConfDayID)
-    REFERENCES ConferenceDays (ConfDayID);
+    REFERENCES ConferenceDays (ConfDayID)
+	ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- End of file.
 
