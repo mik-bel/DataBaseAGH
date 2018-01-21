e9739fab6665870b85b8b804f6d9c1c08fad7bbb
mik-bel <mikita.yakimovich@gmail.com>
2018-01-21 23:53:43 +0100

    transaction to throws

--- Procedures/AddAdress.sql
+++ Procedures/AddAdress.sql
@@ -2,6 +2,7 @@ if EXISTS (select * from sys.procedures where name = 'AddAdress')
     drop procedure AddAdress
 GO
 CREATE PROCEDURE AddAdress
+	@AdressID int = NULL OUT,
 	@Country nvarchar,
 	@PostalCode nvarchar,
 	@City nvarchar,
@@ -9,21 +10,13 @@ CREATE PROCEDURE AddAdress
 	@ScdLine nvarchar
 AS
 BEGIN
-	BEGIN TRANSACTION
+	SET NOCOUNT ON
+	
 	if @Country IS NULL or @PostalCode IS NULL OR @City IS NULL OR @FstLine IS NULL or @ScdLine IS NULL
-	BEGIN
-		RAISERROR ('Musisz podac pelny adres', 14, 1)
-		ROLLBACK TRANSACTION
-		RETURN
-	END
+		THROW 14, 'Musisz podac pelny adres', 1
+
 	INSERT Adresses (Country,PostalCode, City, FstLine, ScdLine)
 	VALUES
 		(@Country, @PostalCode, @City, @FstLine, @ScdLine)
-	IF @@ERROR <>0
-	BEGIN
-		RAISERROR ('Nie mozesz dodac taki adres', 14, 1)
-		ROLLBACK TRANSACTION
-		RETURN
-	END
-	COMMIT TRANSACTION
+	SET @AdressID = SCOPE_IDENTITY(); 
 END
