cd4943fdcc976eddb642e0fcebb74702d7d1984c
mik-bel <mikita.yakimovich@gmail.com>
2018-01-21 23:52:57 +0100

    addConference function created

---
+++ Procedures/AddConference.sql
@@ -0,0 +1,34 @@
+if EXISTS (select * from sys.procedures where name = 'AddConference')
+    drop procedure AddConference
+GO
+CREATE PROCEDURE AddConference
+	@Name varchar,
+	@AdressID int,
+	@ConferenceID int = NULL OUT
+AS
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+		
+		IF @AdressID IS NULL
+			THROW 14,'Musisz podac adres', 1
+		
+		IF @Name IS NULL
+			THROW 14, 'Musisz podac nazwe konferencji', 1
+
+
+		INSERT Conference (Name, AdressID) 
+		VALUES (@Name, @AdressID)
+		SET @ConferenceID =  SCOPE_IDENTITY(); 
+
+		COMMIT TRANSACTION
+	END TRY
+
+	BEGIN CATCH
+		THROW
+		ROLLBACK TRANSACTION
+	END CATCH
+
+END
