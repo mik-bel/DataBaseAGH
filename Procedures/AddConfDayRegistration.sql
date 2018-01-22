fe40d429738ee8c110d32630c4ab46fcf1c8713f
mik-bel <mikita.yakimovich@gmail.com>
2018-01-22 04:06:31 +0100

    AddConfDayRegistration finished

--- Procedures/AddConfDayRegistration.sql
+++ Procedures/AddConfDayRegistration.sql
@@ -19,33 +19,23 @@ BEGIN
 		IF @ConfDayReservationID IS NULL
 			THROW 14,'@ConfDayReservationID is null in AddConfDayRegistration', 1
 		
-		IF @NumReservs IS NULL or @NumReservs < 1
-			THROW 14,'@NumReservs IS NULL or @NumReservs < 1 in AddWorkshopReservation', 1
-		
-		IF @ReservationDate IS NULL 
-			THROW 14,'@ReservationDate is null in AddWorkshopReservation', 1
-		
-		DECLARE @ConfDate date = (select ConferenceDays.Date from Workshops 
-								inner join ConferenceDays on ConferenceDays.ConfDayID = Workshops.ConfDayID
-								where Workshops.WorkshopID=@WorkshopID  )
-				
-		IF @ReservationDate  > @ConfDate
-			THROW 14, '@ReservationDate  > @ConfDate in AddWorkshopReservation', 1
-		
-		
-		IF @NumStudents > @NumReservs
-			THROW 14,'@NumStudents > @NumReservs in AddWorkshopReservation', 1
-		
-		IF @NumStudents < 0
-			THROW 14,'@NumStudents < 0 in AddWorkshopReservation', 1
-	
-		IF @NumReservs > dbo.WorkshopFreeSeats(@WorkshopID)
-			THROW 14,'@NumReservs > dbo.WorkshopFreeSeats(@WorkshopID) in AddWorkshopReservation', 1
+		Declare @ClientID int = (Select TOP 1 ClientID from Participants where Participants.ParticipantID=@ParticipantID)
 
+		IF (SELECT Count (*) from Clients 
+			Inner Join Participants on Participants.ClientID = Clients.ClientID 
+			inner join ConfDayReservations on ConfDayReservations.ClientID = Participants.ClientID 
+			where Participants.ParticipantID = @ParticipantID
+			and ConfDayReservations.ConfDayReservationID = @ConfDayReservationID) = 0
+			THROW 14, 'Nie istnieje takiego polaczenia @ParticipantID - @ConfDayReservationID w AddConfDayRegistration',1
 
-		INSERT WorkshopReservations(WorkshopID, ConfDayReservationID, NumReservs, ReservationDate, NumStudents) 
-		VALUES (@WorkshopID, @ConfDayReservationID, @NumReservs, @ReservationDate, @NumStudents)
-		SET @WorkshopReservationID =  SCOPE_IDENTITY(); 
+		DECLARE @PriceToPay float = dbo.f_PriceToPay (@ClientID)
+
+		IF @PriceToPay >= (select PaidPrice from ConfDayReservations where ConfDayReservationID=@ConfDayReservationID)
+			THROW 14,'Nie zostala dokonana wplata, uczestnik nie moze byc zarejestrowany na konferencje.',1
+
+		INSERT ConfDayRegistrations(ParticipantID, ConfDayReservationID) 
+		VALUES (@ParticipantID, @ConfDayReservationID)
+		SET @ConfDayRegistrationID =  SCOPE_IDENTITY(); 
 
 		COMMIT TRANSACTION
 	END TRY
