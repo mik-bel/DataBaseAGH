b0a03bb757ec04e1b141ba6b620b6631addacde4
nwroblewski <mikolaj.wroblewski1997@gmail.com>
2018-01-22 01:03:05 +0100

    Changed nchar to nvarchar
    In Clients.Login

---
+++ Procedures/ModifyParticipant.sql
@@ -0,0 +1,58 @@
+
+IF OBJECT_ID ('ModifyParticipant') is not null
+DROP PROC ModifyParticipant;
+GO
+
+CREATE PROCEDURE ModifyParticipant (
+	@ParticipantID int,
+	@FirstName nvarchar (20),
+	@LastName nvarchar (20),
+	@AdressID int,
+	@StudentCardID int
+)
+
+AS BEGIN
+	SET NOCOUNT ON
+
+		BEGIN TRY
+			BEGIN TRANSACTION
+
+				IF @ParticipantID is null
+					THROW 2500, 'ParticipantID cant be null', 1
+
+				IF @FirstName is null
+					THROW 2500, 'FirstName cant be null', 1
+
+				IF @LastName is null
+					THROW 2500, 'LastName cant be null', 1
+
+				IF @AdressID is null
+					THROW 2500, 'AdressID cant be null', 1
+
+				IF @StudentCardID is null
+					THROW 2500, 'StudentCardID cant be null', 1
+
+				UPDATE Participants
+				SET
+
+					FirstName = @FirstName
+					LastName = @LastName
+					AdressID = @AdressID
+					StudentCardID = @StudentCardID
+				WHERE ParticipantID = @ParticipantID
+
+				IF @@ROWCOUNT = 0
+					THROW 2500, 'You provided incorrect ParticipantID, there is no participant with such ID', 1
+
+				COMMIT TRANSACTION
+		END TRY
+
+		BEGIN CATCH
+			ROLLBACK TRANSACTION
+			THROW
+		END CATCH
+
+END
+
+GO
+
