ca0f8647a679f85867ef76456b64b6b26e6b80fb
mik-bel <mikita.yakimovich@gmail.com>
2018-01-23 02:35:28 +0100

    3

--- Views/v_ClientsWhoDidntPay.sql
+++ Views/v_ClientsWhoDidntPay.sql
@@ -4,9 +4,11 @@ GO
 
 CREATE VIEW v_ClientsWhoDidntPay
 		AS
-		SELECT * 
+		SELECT Login AS 'Login', ISNULL(CompanyName, 'Personal client') AS 'Company',  dbo.f_PriceToPayPerDay(Clients.ClientID, ConfDayReservationID) - PaidPrice AS 'Price to pay'
 		FROM dbo.Clients
 		INNER JOIN dbo.ConfDayReservations ON ConfDayReservations.ClientID = Clients.ClientID
+		LEFT OUTER JOIN dbo.Companies ON Companies.CompanyID = Clients.CompanyID
 		WHERE dbo.f_PriceToPayPerDay(Clients.ClientID, ConfDayReservationID) >PaidPrice AND 
 		DATEDIFF(DAY, ReservationDate,  GETDATE()) > 7
+		AND Cancelled = 1
 GO
